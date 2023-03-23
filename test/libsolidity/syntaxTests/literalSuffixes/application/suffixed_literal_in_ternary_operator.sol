1e61bec5b61b65236776861e5df3bbbfa580e851
Kamil Śliwak <kamil.sliwak@codepoets.it>
2023-03-23 21:35:31 +0100

    Convert some of the syntax tests into semantic ones

--- test/libsolidity/syntaxTests/literalSuffixes/application/suffixed_literal_in_ternary_operator.sol
+++
@@ -1,5 +0,0 @@
-function suffix(uint) pure suffix returns (bool) {}
-
-contract C {
-    bool x = 1 suffix ? 2 suffix : 3 suffix ? 4 suffix : 5 suffix;
-}
--- test/libsolidity/syntaxTests/literalSuffixes/usableAsSuffix/deprecated_denomination_as_suffix.sol
+++
@@ -1,9 +0,0 @@
-function szabo(uint x) pure suffix returns (uint) { return x; }
-function finney(uint x) pure suffix returns (uint) { return x; }
-
-contract C {
-    function f() public pure {
-        1 szabo;
-        1 finney;
-    }
-}
