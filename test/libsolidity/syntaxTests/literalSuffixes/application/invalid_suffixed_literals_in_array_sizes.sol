c4aee7320f597aa204512a59725547a78880c59f
Kamil Śliwak <kamil.sliwak@codepoets.it>
2023-03-24 14:58:24 +0100

    Tests for qualified suffix use

--- test/libsolidity/syntaxTests/literalSuffixes/application/invalid_suffixed_literals_in_array_sizes.sol
+++ test/libsolidity/syntaxTests/literalSuffixes/application/invalid_suffixed_literals_in_array_sizes.sol
@@ -1,9 +1,14 @@
+==== Source: A.sol ====
 function uintSuffix(uint x) pure suffix returns (uint) {}
 function int8Suffix(uint x) pure suffix returns (int8) {}
 function addressSuffix(uint x) pure suffix returns (address) {}
 function decimalSuffix(uint m, uint e) pure suffix returns (uint) {}
 function stringSuffix(uint x) pure suffix returns (string memory) {}
 function bytesSuffix(uint x) pure suffix returns (bytes memory) {}
+
+==== Source: B.sol ====
+import "A.sol" as A;
+import "A.sol";
 
 contract C {
     uint[42 uintSuffix] a;
@@ -12,6 +17,13 @@ contract C {
     uint[42 decimalSuffix] d;
     uint[42 stringSuffix] e;
     uint[42 bytesSuffix] f;
+
+    uint[42 A.uintSuffix] a;
+    uint[42 A.int8Suffix] b;
+    uint[42 A.addressSuffix] c;
+    uint[42 A.decimalSuffix] d;
+    uint[42 A.stringSuffix] e;
+    uint[42 A.bytesSuffix] f;
 }
 
 contract D {
@@ -23,15 +35,27 @@ contract D {
     uint[bytesSuffix(42)] f;
 }
 // ----
-// TypeError 5462: (408-421): Invalid array length, expected integer literal or constant expression. A suffixed literal is not a constant expression unless the suffix is a denomination.
-// TypeError 5462: (435-448): Invalid array length, expected integer literal or constant expression. A suffixed literal is not a constant expression unless the suffix is a denomination.
-// TypeError 5462: (462-478): Invalid array length, expected integer literal or constant expression. A suffixed literal is not a constant expression unless the suffix is a denomination.
-// TypeError 5462: (492-508): Invalid array length, expected integer literal or constant expression. A suffixed literal is not a constant expression unless the suffix is a denomination.
-// TypeError 5462: (522-537): Invalid array length, expected integer literal or constant expression. A suffixed literal is not a constant expression unless the suffix is a denomination.
-// TypeError 5462: (551-565): Invalid array length, expected integer literal or constant expression. A suffixed literal is not a constant expression unless the suffix is a denomination.
-// TypeError 5462: (595-609): Invalid array length, expected integer literal or constant expression.
-// TypeError 5462: (623-637): Invalid array length, expected integer literal or constant expression.
-// TypeError 5462: (651-668): Invalid array length, expected integer literal or constant expression.
-// TypeError 5462: (682-699): Invalid array length, expected integer literal or constant expression.
-// TypeError 5462: (713-729): Invalid array length, expected integer literal or constant expression.
-// TypeError 5462: (743-758): Invalid array length, expected integer literal or constant expression.
+// DeclarationError 2333: (B.sol:227-250): Identifier already declared.
+// DeclarationError 2333: (B.sol:256-279): Identifier already declared.
+// DeclarationError 2333: (B.sol:285-311): Identifier already declared.
+// DeclarationError 2333: (B.sol:317-343): Identifier already declared.
+// DeclarationError 2333: (B.sol:349-374): Identifier already declared.
+// DeclarationError 2333: (B.sol:380-404): Identifier already declared.
+// TypeError 5462: (B.sol:60-73): Invalid array length, expected integer literal or constant expression. A suffixed literal is not a constant expression unless the suffix is a denomination.
+// TypeError 5462: (B.sol:87-100): Invalid array length, expected integer literal or constant expression. A suffixed literal is not a constant expression unless the suffix is a denomination.
+// TypeError 5462: (B.sol:114-130): Invalid array length, expected integer literal or constant expression. A suffixed literal is not a constant expression unless the suffix is a denomination.
+// TypeError 5462: (B.sol:144-160): Invalid array length, expected integer literal or constant expression. A suffixed literal is not a constant expression unless the suffix is a denomination.
+// TypeError 5462: (B.sol:174-189): Invalid array length, expected integer literal or constant expression. A suffixed literal is not a constant expression unless the suffix is a denomination.
+// TypeError 5462: (B.sol:203-217): Invalid array length, expected integer literal or constant expression. A suffixed literal is not a constant expression unless the suffix is a denomination.
+// TypeError 5462: (B.sol:232-247): Invalid array length, expected integer literal or constant expression. A suffixed literal is not a constant expression unless the suffix is a denomination.
+// TypeError 5462: (B.sol:261-276): Invalid array length, expected integer literal or constant expression. A suffixed literal is not a constant expression unless the suffix is a denomination.
+// TypeError 5462: (B.sol:290-308): Invalid array length, expected integer literal or constant expression. A suffixed literal is not a constant expression unless the suffix is a denomination.
+// TypeError 5462: (B.sol:322-340): Invalid array length, expected integer literal or constant expression. A suffixed literal is not a constant expression unless the suffix is a denomination.
+// TypeError 5462: (B.sol:354-371): Invalid array length, expected integer literal or constant expression. A suffixed literal is not a constant expression unless the suffix is a denomination.
+// TypeError 5462: (B.sol:385-401): Invalid array length, expected integer literal or constant expression. A suffixed literal is not a constant expression unless the suffix is a denomination.
+// TypeError 5462: (B.sol:431-445): Invalid array length, expected integer literal or constant expression.
+// TypeError 5462: (B.sol:459-473): Invalid array length, expected integer literal or constant expression.
+// TypeError 5462: (B.sol:487-504): Invalid array length, expected integer literal or constant expression.
+// TypeError 5462: (B.sol:518-535): Invalid array length, expected integer literal or constant expression.
+// TypeError 5462: (B.sol:549-565): Invalid array length, expected integer literal or constant expression.
+// TypeError 5462: (B.sol:579-594): Invalid array length, expected integer literal or constant expression.
