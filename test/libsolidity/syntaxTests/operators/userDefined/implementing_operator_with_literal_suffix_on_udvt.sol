73783a307d56eb8e2baefbb05948d8fdbf8476db
Matheus Aguiar <matheus.pit@gmail.com>
2023-03-26 15:45:20 -0300

    Adjust test removing invalid case

--- test/libsolidity/syntaxTests/operators/userDefined/implementing_operator_with_literal_suffix_on_udvt.sol
+++ test/libsolidity/syntaxTests/operators/userDefined/implementing_operator_with_literal_suffix_on_udvt.sol
@@ -1,12 +1,7 @@
 type Int is int;
-using {add as +, unsub as -} for Int global;
-
-function add(Int x, Int y) pure suffix returns (Int) {
-    return Int.wrap(Int.unwrap(x) + Int.unwrap(y));
-}
+using {unsub as -} for Int global;
 
 function unsub(Int x) pure suffix returns (Int) {
     return Int.wrap(-Int.unwrap(x));
 }
 // ----
-// TypeError 1587: (75-89): Literal suffix function has invalid parameter types. The mantissa parameter must be an integer. The exponent parameter must be an unsigned integer.
